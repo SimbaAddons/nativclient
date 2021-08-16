f864552969fc953d81b8df5b8fbb66fec1146c99
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2021-08-16 22:33:35 +0200

    [+] climb-hero

---
+++ spiele/climb-hero.js
@@ -0,0 +1,6 @@
+// Importiere das Script
+$gameload = (https://nativclint.black-host.eu/source/spiel/climb-hero)
+$serv = (https://nativclint.black-host.eu/source/load/serv)
+
+select * from(../source/gamebase.js)
+end;
