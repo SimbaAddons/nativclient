aa6a9a98a0562fc0b4f236b4068541b4755eb576
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2021-08-16 22:25:53 +0200

    [+] Uploaded DoodleJump

--- spiele/doodle-jump.js
+++ spiele/doodle-jump.js
@@ -1,1 +1,16 @@
+// Importiere das Script
+$gameload = (https://nativclint.black-host.eu/source/spiel/doodle-jump)
+$serv = (https://nativclint.black-host.eu/source/load/serv)
 
+// Lade die Spielcookies von der IP
+$game = get(ip)
+   load * from serv(cache)
+sleep(?) while load;
+
+// PHP Text
+<php>
+<iframe src="$gameload" datasrc="$game"></iframe>
+<?php>
+
+catch($error);
+serv.console display $error
