01a8537afc7af69153c79ef7c09f65872f30543f
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2021-08-16 22:37:01 +0200

    [+] star-battles

---
+++ spiele/star-battles.js
@@ -0,0 +1,6 @@
+// Importiere das Script
+$gameload = (https://nativclint.black-host.eu/source/spiel/star-battles)
+$serv = (https://nativclint.black-host.eu/source/load/serv)
+
+select * from(../source/gamebase.js)
+end;
