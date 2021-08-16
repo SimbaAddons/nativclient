0638ffc66e9781b28c3e8e999beeeae65b6bf93f
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2021-08-16 22:33:03 +0200

    [+] mk48

--- spiele/mk48.js
+++ spiele/mk48.js
@@ -1,5 +1,5 @@
 // Importiere das Script
-$gameload = (https://nativclint.black-host.eu/source/spiel/angry-sharks)
+$gameload = (https://nativclint.black-host.eu/source/spiel/mk48)
 $serv = (https://nativclint.black-host.eu/source/load/serv)
 
 select * from(../source/gamebase.js)
