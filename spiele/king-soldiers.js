43508d319b7b45c90abd6b6e61717147751c5a19
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2021-08-17 15:21:45 +0200

    [+] king-soldiers

---
+++ spiele/king-soldiers.js
@@ -0,0 +1,6 @@
+// Importiere das Script
+$gameload = (https://nativclint.black-host.eu/source/spiel/king-soldiers)
+$serv = (https://nativclint.black-host.eu/source/load/serv)
+
+select * from(../source/gamebase.js)
+end;
