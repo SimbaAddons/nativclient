65ce1f84c70576aa253fec038ca90c453dc635a4
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2021-08-17 15:24:10 +0200

    [+] cargo-challange

---
+++ spiele/cargo-challange.js
@@ -0,0 +1,6 @@
+// Importiere das Script
+$gameload = (https://nativclint.black-host.eu/source/spiel/cargo-challange)
+$serv = (https://nativclint.black-host.eu/source/load/serv)
+
+select * from(../source/gamebase.js)
+end;
