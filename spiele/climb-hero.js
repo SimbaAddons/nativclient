75083213acbeee528a609704d51ca82b5eb6ede9
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2021-08-17 15:27:15 +0200

    Delete climb-hero.js

--- spiele/climb-hero.js
+++
@@ -1,6 +0,0 @@
-// Importiere das Script
-$gameload = (https://nativclint.black-host.eu/source/spiel/climb-hero)
-$serv = (https://nativclint.black-host.eu/source/load/serv)
-
-select * from(../source/gamebase.js)
-end;
