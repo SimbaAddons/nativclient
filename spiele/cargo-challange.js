0b0bbcbac1a7fa70e0631d1f9e993f434bcf0488
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2021-08-17 15:27:09 +0200

    Delete cargo-challange.js

--- spiele/cargo-challange.js
+++
@@ -1,6 +0,0 @@
-// Importiere das Script
-$gameload = (https://nativclint.black-host.eu/source/spiel/cargo-challange)
-$serv = (https://nativclint.black-host.eu/source/load/serv)
-
-select * from(../source/gamebase.js)
-end;
